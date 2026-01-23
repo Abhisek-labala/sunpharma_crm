03a8fa39a9910101693ddb26790b6c2c2dede737
Abhisek Labala <abhisek.labala@cordeliatech.com>
2026-01-23 13:32:17 +0530

    implemented

--- app/Http/Controllers/FeedBackController.php
+++ app/Http/Controllers/FeedBackController.php
@@ -76,36 +76,51 @@ class FeedBackController extends Controller
 }
    public function followupFormmis(Request $request)
 {
-    $id = $request->get('patient_id'); // get query param
-    $day3_data_exists = false;
-    $day7_data_exists = false;
-    $day15_data_exists = false;
-    $day30_data_exists = false;
-    $day45_data_exists = false;
-    $day60_data_exists = false;
-    $day90_data_exists = false;
-    $day120_data_exists = false;
-    $day150_data_exists = false;
-    $day180_data_exists = false;
+    try {
+        $id = $request->get('patient_id'); // get query param
+        
+        if (!$id) {
+            return response()->json(['error' => 'Patient ID is required'], 400);
+        }
+        
+        $day3_data_exists = false;
+        $day7_data_exists = false;
+        $day15_data_exists = false;
+        $day30_data_exists = false;
+        $day45_data_exists = false;
+        $day60_data_exists = false;
+        $day90_data_exists = false;
+        $day120_data_exists = false;
+        $day150_data_exists = false;
+        $day180_data_exists = false;
 
-    $submitData = FeedbackSubmitted::where('patient_id', $id)
-    ->select('day')
-    ->distinct()
-    ->orderByRaw('CAST(day AS INTEGER) ASC')
-    ->get();
+        $submitData = FeedbackSubmitted::where('patient_id', $id)
+        ->select('day')
+        ->distinct()
+        ->orderByRaw('CAST(day AS INTEGER) ASC')
+        ->get();
 
-    return view('mis.followupForm', ['patientId' => $id, 'day3_data_exists' => $day3_data_exists,
-    'day7_data_exists' => $day7_data_exists,
-    'day15_data_exists' => $day15_data_exists,
-    'day30_data_exists' => $day30_data_exists,
-    'day45_data_exists' => $day45_data_exists,
-    'day60_data_exists' => $day60_data_exists,
-    'day90_data_exists' => $day90_data_exists,
-    'day120_data_exists' => $day120_data_exists,
-    'day150_data_exists' => $day150_data_exists,
-    'day180_data_exists' => $day180_data_exists,
+        return view('mis.followupForm', ['patientId' => $id, 'day3_data_exists' => $day3_data_exists,
+        'day7_data_exists' => $day7_data_exists,
+        'day15_data_exists' => $day15_data_exists,
+        'day30_data_exists' => $day30_data_exists,
+        'day45_data_exists' => $day45_data_exists,
+        'day60_data_exists' => $day60_data_exists,
+        'day90_data_exists' => $day90_data_exists,
+        'day120_data_exists' => $day120_data_exists,
+        'day150_data_exists' => $day150_data_exists,
+        'day180_data_exists' => $day180_data_exists,
 
-]);
+    ]);
+    } catch (\Exception $e) {
+        \Log::error('Follow up Form MIS Error: ' . $e->getMessage());
+        \Log::error('Stack trace: ' . $e->getTraceAsString());
+        
+        return response()->view('errors.500', [
+            'error' => 'An error occurred while loading the follow-up form',
+            'message' => $e->getMessage()
+        ], 500);
+    }
 }
    public function followupFormeducator(Request $request)
 {
