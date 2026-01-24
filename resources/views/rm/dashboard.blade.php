d9939fd1621521d43e1f4c5e9b133ad904ef3d12
Abhisek Labala <abhisek.labala@cordeliatech.com>
2026-01-24 23:54:53 +0530

    added digi educator code

--- resources/views/rm/dashboard.blade.php
+++ resources/views/rm/dashboard.blade.php
@@ -74,8 +74,8 @@
                                 <th>BMI</th>
                                 <th>Consent Form</th>
                                 <th>Prescription</th>
-                                <th>Educator Name</th>
-                                <th>RM Name</th>
+                                <th>Counsellor Name</th>
+                                <th>RC Name</th>
                                 <th>City</th>
                             </tr>
                         </thead>
